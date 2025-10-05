54fdcdde7e67b508d69802ee3a3e8dc740e990e2
Serdar Elibol <elibol386@gmail.com>
2025-10-05 17:47:59 +0300

    Dosya isimlerinin SEO uyumlu düzeltilmesi

--- Core/ETicaretAPI.Application/Services/IFileService.cs
+++ Core/ETicaretAPI.Application/Services/IFileService.cs
@@ -4,8 +4,7 @@ namespace ETicaretAPI.Application.Services
 {
     public interface IFileService
     {
-        Task<List<(string FileName, string path)>> UploadAsync(string path, IFormFileCollection files);
-        Task<string> FileRenameAsync(string fileName);
+        Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files);
         Task<bool> CopyFileAsync(string path, IFormFile file);
     }
 }
